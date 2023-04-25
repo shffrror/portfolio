ad466789c8faf00f354ec0a06b8c5dc28ca32603
shffrror <shffrror@pm.me>
2023-04-25 10:55:24 -0400

    Starting to work on checkers board

--- checkers/game.py
+++ checkers/game.py
@@ -1,7 +1,7 @@
 import pygame
 pygame.init()
 
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([800, 800])
 
 running = True
 while running:
@@ -12,6 +12,21 @@ while running:
     screen.fill((255, 255, 255))
 
     pygame.draw.circle(screen, (0, 0, 0), (250, 250), 75)
+
+    starting_x = 0
+    starting_y = 0
+    red_black_bool_track = 0
+    while(starting_x <= 800):
+        if red_black_bool_track == 0:
+            pygame.draw.rect(screen, (255, 255, 255), (starting_x, starting_y, 100, 100))
+        else:
+            pygame.draw.rect(screen, (0, 0, 0), (starting_x, starting_y, 100, 100))
+
+        red_black_bool_track = not red_black_bool_track
+        starting_x += 10
+        starting_y += 10
+
+    # pygame.draw.rect(window, color, (x, y, width, height)
 
     pygame.display.flip()
 
